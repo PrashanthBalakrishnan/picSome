afa69a231949a69281589d501a5858ce72dc5b6e
Prashanth Balakrishnan <prashbala13@gmail.com>
2022-10-13 10:37:22 -0400

    added outline of the site

---
+++ picsome/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './components/Header'
+import Cart from './pages/Cart'
+import Photos from './pages/Photos'
+
+
+export default function App() {
+  return (
+    <div>
+      <Header/>
+      <h1>Home Page</h1>
+    </div>
+  )
+}
